8705af92da87bd3f9719b3cff5053d007d945845
sammychill17 <shamaylchy@gmail.com>
2024-11-01 18:00:30 -0300

    additional animations, assets related properties

--- Assets/PlayerScripts/PlayerMovement.cs
+++ Assets/PlayerScripts/PlayerMovement.cs
@@ -4,18 +4,41 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     private Animator animator;
+    public Transform cameraTransform;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        if (Camera.main != null)
+        {
+            cameraTransform = Camera.main.transform;
+        }
     }
 
     void Update()
     {
-        float move = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
 
-        animator.SetFloat("Speed", Mathf.Abs(move));
-        Vector3 movement = new Vector3(0, 0, move) * speed * Time.deltaTime;
-        transform.Translate(movement, Space.Self);
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            transform.Translate(moveDir * speed * Time.deltaTime, Space.World);
+
+            animator.SetFloat("Speed", direction.magnitude);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("Jump");
+        }
     }
 }
