a465a9efc63eb0ea52de4bb9a44490b894fc4bca
Matthew Margeson <margesonmj@dal.ca>
2024-11-12 20:47:26 -0400

    separated ItemVisuals and ItemPickup logic

--- Assets/Scripts/ItemScripts/ItemPickup.cs
+++ Assets/Scripts/ItemScripts/ItemPickup.cs
@@ -4,42 +4,26 @@ using UnityEngine;
 
 public class ItemPickup : MonoBehaviour
 {
-    public Item Item;
-    private bool isPlayerNearby = false;
+    public Item      Item;
+    private bool     playerNearby = false;
     private Animator playerAnimator;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log($"OnTriggerEnter called by: {other.name}");
 
-        if (other.CompareTag("Player"))
-        {
-            isPlayerNearby = true;
-            playerAnimator = other.GetComponent<Animator>();
-            Debug.Log("Player is near the item.");
-        }
+    private void Start()
+    {
+        GetComponentInChildren<ItemVisuals>().SetItemPickup(this);
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        Debug.Log($"OnTriggerExit called by: {other.name}");
-
-        if (other.CompareTag("Player"))
-        {
-            isPlayerNearby = false;
-            playerAnimator = null;
-            Debug.Log("Player left the item.");
-        }
-    }
 
     void Update()
     {
-        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
+        if (playerNearby && Input.GetKeyDown(KeyCode.E))
         {
             Debug.Log("E key pressed, attempting to pick up item.");
             Pickup();
         }
     }
+
 
     void Pickup()
     {
@@ -52,5 +36,17 @@ public class ItemPickup : MonoBehaviour
         Debug.Log($"Picking up item: {Item.name}");
         InventoryManager.Instance.Add(Item);
         Destroy(gameObject);
+    }
+
+
+    public void SetPlayerNearby(bool playerNearby)
+    {
+        this.playerNearby = playerNearby;
+    }
+
+
+    public void SetPlayerAnimator(Animator playerAnimator)
+    {
+        this.playerAnimator = playerAnimator;
     }
 }
