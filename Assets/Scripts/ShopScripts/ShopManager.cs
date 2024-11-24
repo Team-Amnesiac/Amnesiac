1c1a8bc0a0d24e78419d6adaba5b448fb1441f56
sammychill17 <shamaylchy@gmail.com>
2024-11-24 01:01:49 -0400

    Delegated UI variables and methods to ShopUI from ShopManager

--- Assets/Scripts/ShopScripts/ShopManager.cs
+++ Assets/Scripts/ShopScripts/ShopManager.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
 using System.Collections.Generic;
 
 public class ShopManager : MonoBehaviour
@@ -8,20 +6,13 @@ public class ShopManager : MonoBehaviour
     [SerializeField] private List<Item> shopItems; // ScriptableObject Items available in the shop
     [SerializeField] private GameObject shopItemPrefab; // Prefab for each ShopItemButton
     [SerializeField] private Transform shopItemContainer; // Parent object for dynamically created ShopItemButtons
-    [SerializeField] private TextMeshProUGUI totalCostText; // Displays the total cost
-    [SerializeField] private TextMeshProUGUI playerCurrencyText; // Displays the player's current currency
-    [SerializeField] private Button buyButton; // Finalizes purchases
 
+    private List<Item> selectedItems = new List<Item>(); // Tracks the items the player has selected
     private int totalCost = 0;
-    private List<Item> selectedItems = new List<Item>(); // tracks the items the player has selected
 
     private void Start()
     {
         PopulateShop();
-        UpdatePlayerCurrency();
-        UpdateTotalCost();
-
-        buyButton.onClick.AddListener(FinalizePurchase);
     }
 
     private void PopulateShop()
@@ -32,7 +23,7 @@ public class ShopManager : MonoBehaviour
 
             ShopItemController itemController = shopItem.GetComponent<ShopItemController>();
 
-            // set up the shop item UI using the ScriptableObject data
+            // Set up the shop item UI using the ScriptableObject data
             itemController.SetUp(item, this);
         }
     }
@@ -41,33 +32,25 @@ public class ShopManager : MonoBehaviour
     {
         totalCost += item.value;
         selectedItems.Add(item);
-        UpdateTotalCost();
+        ShopUI.Instance.UpdateTotalCost(totalCost);
     }
 
     public void RemoveFromTotalCost(Item item)
     {
         totalCost -= item.value;
         selectedItems.Remove(item);
-        UpdateTotalCost();
+        ShopUI.Instance.UpdateTotalCost(totalCost);
     }
 
-    private void UpdateTotalCost()
+    public void FinalizePurchase()
     {
-        totalCostText.text = $"Total Cost: {totalCost}";
-    }
+        int playerCurrency = PlayerManager.Instance.GetCurrency();
 
-    private void UpdatePlayerCurrency()
-    {
-        playerCurrencyText.text = $"Currency: {PlayerManager.Instance.GetCurrency()}";
-    }
-
-    private void FinalizePurchase()
-    {
-        if (PlayerManager.Instance.GetCurrency() >= totalCost)
+        if (playerCurrency >= totalCost)
         {
-            // deduct the total cost from the player's currency
+            // Deduct the total cost from the player's currency
             PlayerManager.Instance.SpendCurrency(totalCost);
-            UpdatePlayerCurrency();
+            ShopUI.Instance.UpdatePlayerCurrency(playerCurrency - totalCost);
 
             // Add purchased items to the player's inventory
             foreach (Item item in selectedItems)
@@ -76,14 +59,14 @@ public class ShopManager : MonoBehaviour
                 Debug.Log($"Purchased: {item.itemName}");
             }
 
-            // reset the shop selections
+            // Reset the shop selections
             totalCost = 0;
             selectedItems.Clear();
-            UpdateTotalCost();
+            ShopUI.Instance.UpdateTotalCost(totalCost);
         }
         else
         {
-            Debug.Log("Not enough currency!");
+            ShopUI.Instance.ShowNotEnoughCurrencyWarning();
         }
     }
 }
--- Assets/Scripts/UIScripts/ShopUI.cs
+++ Assets/Scripts/UIScripts/ShopUI.cs
@@ -1,12 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class ShopUI : MonoBehaviour
 {
+    public static ShopUI Instance;
+
+    [SerializeField] private TextMeshProUGUI totalCostText;
+    [SerializeField] private TextMeshProUGUI playerCurrencyText;
+    [SerializeField] private Button buyButton;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     void Start()
     {
         UIManager.Instance.setUI(UIManager.UI.Shop, this);
         UIManager.Instance.hideUI(UIManager.UI.Shop);
+        buyButton.onClick.AddListener(() =>
+        {
+            if (ShopManager.Instance != null)
+            {
+                ShopManager.Instance.FinalizePurchase();
+            }
+        });
+
+        UpdatePlayerCurrency(PlayerManager.Instance.GetCurrency());
+        UpdateTotalCost(0);
+    }
+
+    ///
+    /// Updates the total cost display in the shop UI.
+    ///
+    public void UpdateTotalCost(int totalCost)
+    {
+        totalCostText.text = $"Total Cost: {totalCost}";
+    }
+
+    ///
+    /// Updates the player's currency display in the shop UI.
+    ///
+    public void UpdatePlayerCurrency(int currency)
+    {
+        playerCurrencyText.text = $"Currency: {currency}";
+    }
+
+    ///
+    /// Displays a warning if the player doesn't have enough currency, could have an additional UI element
+    ///
+    public void ShowNotEnoughCurrencyWarning()
+    {
+        Debug.Log("Not enough currency!"); // Placeholder for an actual UI warning
+        // can add a UI popup or highlight the issue visually here if needed, but not necessary.
     }
 }
