98c164b20ad807cc8ba24019eeca22ea5177b6b3
anastasiyagur <anastasiyagur25@gmail.com>
2024-11-30 23:11:39 -0400

    deleted testplayer script, added comments on scripts fixed platform animation

--- Assets/Scripts/DoorScript/Door.cs
+++ Assets/Scripts/DoorScript/Door.cs
@@ -2,35 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This Door script handles the opening and closing of a door using animations when a player enters or exits a trigger area
 public class Door : MonoBehaviour
 {
+    // The Animator components for the right and left doors
     public Animator rightDoorAnimator;
     public Animator leftDoorAnimator;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    // called when another collider enters the trigger collider attached to this object
     private void OnTriggerEnter(Collider other)
     {
+        // Set the isOpen bool parameter of both animators to true to open the doors.
         rightDoorAnimator.SetBool("isOpen", true);
         leftDoorAnimator.SetBool("isOpen", true);
     }
 
+    // on trigger exit is called when another collider exits the trigger collider attached to this object
     private void OnTriggerExit(Collider other)
     {
+        // Set the isOpen bool parameter of both animators to false to close the doors
         rightDoorAnimator.SetBool("isOpen", false);
         leftDoorAnimator.SetBool("isOpen", false);
-
-
     }
-
 }
--- Assets/Scripts/PlatformScript/PlatformAttach.cs
+++ Assets/Scripts/PlatformScript/PlatformAttach.cs
@@ -2,28 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// this script attaches the player to a moving platform when they enter a trigger zone, and detaches them when they exit that zone
 public class PlatformAttach : MonoBehaviour
 {
+    // The player GameObject that will be attached to the platform
     public GameObject Player;
 
+    // is called when another collider enters the trigger collider attached to this object
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(Player);
-
+        // Check if the object entering the trigger is the player
         if (other.gameObject == Player)
         {
-            Debug.Log(Player + "Player entered the trigger zone.");
+            // Set the player's parent to the platform to make the player move with the platform
             Player.transform.parent = transform;
         }
     }
 
+    // this is called when another collider exits the trigger collider attached to this object
     private void OnTriggerExit(Collider other)
     {
+        // will check if the object exiting the trigger is the player
         if (other.gameObject == Player)
         {
-            Debug.Log(Player + "Player exited the trigger zone.");
+            // detach the player from the platform
             Player.transform.parent = null;
         }
     }
-
 }
